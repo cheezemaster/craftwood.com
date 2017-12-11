63da3d10b45421240cff7a4ab63a65824e4f67ff
cheezemaster <34454616+cheezemaster@users.noreply.github.com>
2017-12-11 21:29:30 +0200

    main

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'craftwood');
+define('DB_NAME', 'craftwood1');
 
 /** MySQL database username */
 define('DB_USER', 'root');
