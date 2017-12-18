4217d19302f20ee62fbe304f4904d6f3cc54cc10
cheezemaster <34454616+cheezemaster@users.noreply.github.com>
2017-12-18 12:44:14 +0200

    main

--- wp-content/themes/ss/templates/home.php
+++ wp-content/themes/ss/templates/home.php
@@ -249,6 +249,38 @@
       </div>
     </section>
 
+    <section class="secondd" id="services">
+      <div class="container inner inner2">
+
+        <div class="row text-center">
+          <div class="col-md-4 bglo han serbg seric1">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary srva"></i>
+              <i class="fa fa-shopping-cart fa-stack-1x fa-inverse srva"></i>
+            </span>
+            <h4 class="service-heading">E-Commerce</h4>
+            <p class="text-muted seer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+          </div>
+          <div class="col-md-4 bglo han serbg seric2">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary srva"></i>
+              <i class="fa fa-laptop fa-stack-1x fa-inverse srva"></i>
+            </span>
+            <h4 class="service-heading">Design</h4>
+            <p class="text-muted seer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+          </div>
+          <div class="col-md-4 bglo han serbg seric3">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x text-primary srva"></i>
+              <i class="fa fa-lock fa-stack-1x fa-inverse srva"></i>
+            </span>
+            <h4 class="service-heading">Web</h4>
+            <p class="text-muted seer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <!-- About -->
     <!-- <section id="about">
       <div class="container inner onas">
