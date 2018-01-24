7021d6d0636d7691fd7d1e67dd9df1d02b81e6c9
cheezemaster <34454616+cheezemaster@users.noreply.github.com>
2018-01-24 06:49:04 +0200

    main

--- wp-content/themes/ss/templates/home.php
+++ wp-content/themes/ss/templates/home.php
@@ -61,9 +61,9 @@
     </div>
       </div>
       <div class="container inner server">
-<div class="logoicon">
-        <img class="logo" src="/img/images/logo5.png">
-      </div>
+
+        <img class="logo" src="/img/images/logo4.png">
+
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
